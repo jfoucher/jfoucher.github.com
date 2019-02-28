630e58c8650bb94113f623608a1462c6747a7fca
Jonathan Foucher <jfoucher@gmail.com>
2019-02-28 14:39:52 +0100

    publish

---
+++ projects/aa.sol
@@ -0,0 +1,14 @@
+contract crypTama {
+    struct Tama {
+        uint dna;
+        uint owner;
+    }
+    struct Toy {
+        uint type;
+        string name;
+        uint owner;
+    }
+    //TODO can buy and sell Tamas
+    //TODO can buy and sell Toys
+    //TODO they should be able to breed
+}
